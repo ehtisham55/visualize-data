57242c67fcaad6e02458ae4ac6fae631da945dad
ehtisham55 <75329173+ehtisham55@users.noreply.github.com>
2020-12-24 18:20:38 +0500

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv("Iris_1.csv")
+
+#SepalLengthCm Vs Species (line graph).
+# plt.plot(df["SepalLengthCm"], df["Species"])
+# plt.title("line Graph ")
+# plt.xlabel("SepalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalLengthCm Vs Species (bar graph).
+# plt.bar(df["SepalLengthCm"], df["Species"])
+# plt.title("bar Graph ")
+# plt.xlabel("SepalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalLengthCm Vs Species (scatter graph).
+# plt.scatter(df["SepalLengthCm"], df["Species"])
+# plt.title("scatter Graph ")
+# plt.xlabel("SepalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalWidthCm Vs Species (line graph).
+# plt.plot(df["SepalWidthCm"], df["Species"])
+# plt.title("line Graph ")
+# plt.xlabel("SepalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalWidthCm Vs Species (bar graph).
+# plt.bar(df["SepalWidthCm"], df["Species"])
+# plt.title("bar Graph ")
+# plt.xlabel("SepalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalWidthCm Vs Species (scatter graph).
+# plt.scatter(df["SepalWidthCm"], df["Species"])
+# plt.title("scatter Graph ")
+# plt.xlabel("SepalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#PetalLengthCm Vs Species (line graph).
+# plt.plot(df["PetalLengthCm"], df["Species"])
+# plt.title("line Graph ")
+# plt.xlabel("PetalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#PetalLengthCm Vs Species (bar graph).
+# plt.bar(df["PetalLengthCm"], df["Species"])
+# plt.title("bar Graph ")
+# plt.xlabel("PetalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+# PetalLengthCm Vs Species (scatter graph).
+# plt.scatter(df["PetalLengthCm"], df["Species"])
+# plt.title("scatter Graph ")
+# plt.xlabel("PetalLengthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#PetalWidthCm Vs Species (bar graph).
+# plt.bar(df["PetalWidthCm"], df["Species"])
+# plt.title("bar Graph ")
+# plt.xlabel("PetalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#PetalWidthCm Vs Species (bar graph).
+# plt.scatter(df["PetalWidthCm"], df["Species"])
+# plt.title("scatter Graph ")
+# plt.xlabel("PetalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm Vs Species (line graph).
+# plt.plot(df["SepalLengthCm"], df["Species"] ,color='red',label='line1')
+# plt.plot(df["SepalWidthCm"], df["Species"],color='blue',label='line2')
+# plt.plot(df["PetalLengthCm"], df["Species"],color='yellow',label='line3')
+# plt.plot(df["PetalWidthCm"], df["Species"],color='black',label='line4')
+# plt.title("line Graph ")
+# plt.xlabel("SepalLengthCm , SepalWidthCm , PetalLengthCm , PetalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm Vs Species (bar graph).
+# plt.bar(df["SepalLengthCm"], df["Species"] ,color='red',label='line1')
+# plt.bar(df["SepalWidthCm"], df["Species"],color='blue',label='line2')
+# plt.bar(df["PetalLengthCm"], df["Species"],color='yellow',label='line3')
+# plt.bar(df["PetalWidthCm"], df["Species"],color='black',label='line4')
+# plt.title("bar Graph ")
+# plt.xlabel("SepalLengthCm , SepalWidthCm , PetalLengthCm , PetalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
+#SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm Vs Species (scatter graph).
+# plt.scatter(df["SepalLengthCm"], df["Species"] ,color='red',label='line1')
+# plt.scatter(df["SepalWidthCm"], df["Species"],color='blue',label='line2')
+# plt.scatter(df["PetalLengthCm"], df["Species"],color='yellow',label='line3')
+# plt.scatter(df["PetalWidthCm"], df["Species"],color='black',label='line4')
+# plt.title("scatter Graph ")
+# plt.xlabel("SepalLengthCm , SepalWidthCm , PetalLengthCm , PetalWidthCm")
+# plt.ylabel("Species")
+# plt.show()
+
